f8357dcea53e5e30573f348d9e7e8d96d5aab2c2
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-15 15:02:03 +0700

    day 6

--- Ref/Program.cs
+++ Ref/Program.cs
@@ -1,14 +1,13 @@
-﻿class Program
-{
-	static vois Main()
-	{
+﻿class Program {
+	static void Main() {
 		int a = 2;
 		Adder add = new();
 		add.Add(ref a);
 		Console.WriteLine(a);
 	}
 }
-class Adder
-{
-	public void Add()
+class Adder {
+	public void Add(ref int x) {
+		x++;
+	}
 }
