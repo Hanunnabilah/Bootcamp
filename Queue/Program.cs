e9c85dcf33b8c4ed3587e12c52801a283695272a
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-18 09:42:40 +0700

    Queue

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -1,7 +1,20 @@
-﻿class Program
+﻿using System.Runtime.InteropServices.Marshalling;
+
+class Program
 {
 	static void Main()
 	{
+		// create queue for fruits
+		Queue<string> fruits = new Queue<string>();
 		
+		// add Lemon and Watermelon to the queue
+		fruits.Enqueue("Lemon");
+		fruits.Enqueue("Watermelon");
+		
+		// print elements of the queue
+		foreach(string i in fruits)
+		{
+			Console.WriteLine(i);
+		}
 	}
 }
