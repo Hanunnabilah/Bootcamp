c7ee7320e113d796a35a53f982b12404393d59c8
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-25 11:03:18 +0700

    Finalizer Inheritance

---
+++ FinalizerInheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿﻿class Aa 
+{
+	~Aa() 
+	{
+		Console.WriteLine("Aa destructor");
+	}
+}
+class Bb : Aa
+{
+	~Bb() 
+	{
+		Console.WriteLine("Bb destructor");
+	}
+}
+class Cc : Bb
+{
+	~Cc() 
+    {
+        Console.WriteLine("Cc destructor");
+    }
+}
+
+class Program 
+{
+	static void Main() 
+	{
+		InstanceCreator();
+		GC.Collect();
+		GC.WaitForPendingFinalizers();
+	}
+	static void InstanceCreator() 
+	{
+		Cc c = new();
+	}
+}
