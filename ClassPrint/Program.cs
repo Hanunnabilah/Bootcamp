8f587fa9153653adb673b8cab3882f90c8d28503
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-25 14:39:45 +0700

    Add ToString Object

---
+++ ClassPrint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+class Program
+{
+	static void Main()
+	{
+		Car car = new();
+		car.name = "Hyundai";
+		Console.WriteLine(car);
+	}
+}
+class Car
+{
+	public string name;
+    public override string ToString() // this is override from parent class object
+    {
+        return name;
+    }
+}
