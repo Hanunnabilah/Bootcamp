bddb6a70bca2180552613a084cbf712b00cc5f28
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-25 14:07:34 +0700

    Class without Finalizer

---
+++ ClassWithoutFinalizer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+class Aa()
+{
+
+}
+class Bb()
+{
+	
+}
+class Cc()
+{
+
+}
+
+class Program
+{
+	static void Main()
+	{
+		int iteration = 1_000_000;
+		Stopwatch stopWatch = new();
+		stopWatch.Start();
+		for(int i = 0; i < iteration;i++)
+		{
+			Aa a = new Aa();
+			Bb b = new Bb();
+			Cc c = new Cc();
+		}	
+		stopWatch.Stop();
+		Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+	}
+}
