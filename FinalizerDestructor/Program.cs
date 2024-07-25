64a11f9ebb2446ca7e8734cb88b2a3903579ec13
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-25 10:40:32 +0700

    Finalizer or Destructor

---
+++ FinalizerDestructor/Program.cs
@@ -0,0 +1,25 @@
+﻿// Finalizer or Destructor 
+class Destructor
+{
+	public Destructor ()
+	{
+		Console.WriteLine($"This is Constructor {GC.GetGeneration(this)} created");
+	}
+	~Destructor ()
+	{
+		Console.WriteLine($"This is Destructor {GC.GetGeneration(this)} created");
+	}
+}
+class Program
+{
+	static void Main()
+	{
+		instanceCreator();
+		GC.Collect ();
+		GC.WaitForPendingFinalizers ();
+	}	
+	static void instanceCreator()
+	{
+		Destructor myDestructor = new Destructor ();
+	}
+}
