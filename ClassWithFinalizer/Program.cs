45c6ef25335ddcce0e7c9f964930909ded75ff22
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-25 11:02:56 +0700

    ClassWithFinalizer

---
+++ ClassWithFinalizer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+class A()
+{
+	~A () {}
+}
+class B()
+{
+	~B () {}
+}
+class C()
+{
+	~C () {}
+}
+
+class Program
+{
+	static void Main()
+	{
+		int iteration = 1_000_000;
+		Stopwatch stopWatch = new();
+		stopWatch.Start();
+		for(int i = 0; i < iteration;i++)
+		{
+			A a = new A();
+			B b = new B();
+			C c = new C();
+		}	
+		stopWatch.Stop();
+		Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+	}
+}
