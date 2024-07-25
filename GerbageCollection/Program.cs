e6b92a0b430afeae604208cf892cf97a44f49de9
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-25 08:08:30 +0700

    GarbageCollection

---
+++ GerbageCollection/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿using System.Text;
+class Program 
+{
+	static void Main() {
+		StringBuilder Hello = new("Hello");
+		int iteration = 100;
+		
+		for(int i = 0;i < iteration;i++) 
+		{
+			Hello.Append("A");
+			Hello.Append("!");
+			Hello.Replace("e", "c");
+			Thread.Sleep(2);
+		}
+	} 
+}
