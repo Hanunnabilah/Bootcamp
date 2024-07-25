27dffc41fc63f8bc7d82cf84c7d7255bce80e5e3
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-25 14:06:36 +0700

    DIspose Using

---
+++ Using/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+class Program
+{
+	static void Main()
+	{
+		MyMethod();
+	}
+	static void MyMethod()
+	{
+		using(FileStream fs = new FileStream())
+		{
+		AnotherMethod(fs);
+		// fs.Dispose();
+		}
+	}
+	static void AnotherMethod(FileStream fs)
+	{
+		
+	}
+}
