d55360df94dce60be2644f0112e6c9ae46c1f424
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-26 14:11:34 +0700

    Debugging Calculator

---
+++ DebuggingCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿class Calculator
+{
+	public int Add(int a, int b)
+	{
+		return a+b;
+	}
+}
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Start");
+		Calculator calculator = new();
+		int result = calculator.Add(5, 5);
+		Console.WriteLine(result);
+	}
+}
