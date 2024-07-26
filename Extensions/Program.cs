820ee9e3c06e123f92c906e160188a5d5fc0bccc
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-26 09:46:35 +0700

    Add Extensions

---
+++ Extensions/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program 
+{
+	static void Main()
+	{
+		string a = "Good Morning";
+		a.Cetak();
+		int x = 24;
+		x.Cetak();
+		float[] myFloat = {1f,1f,1f,1f,1f};
+		myFloat.Cetak();
+	}
+}
+public static class myExtensions
+{
+	public static void Cetak(this object value)
+	{
+		Console.WriteLine(value);
+	}
+}
