44d8a0dda2786b93316b691b6721c33aca60eee0
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-26 09:48:17 +0700

    Question of Abstraction

---
+++ Partial/Program.cs
@@ -0,0 +1,19 @@
+﻿class KakekNenek
+{
+	public string name = "Kakek dan Nenek";
+}
+class BapakIbu : KakekNenek 
+{
+	public int age = 7575;
+}
+class Anak : BapakIbu
+{
+	public string nama = "Anakmu";
+}
+class Program
+{
+	static void Main()
+	{
+		
+	}
+}
