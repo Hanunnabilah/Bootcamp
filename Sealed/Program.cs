68eba9e09e002bcf45f42d16234b9ce54c979667
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-26 09:56:26 +0700

    Sealed

---
+++ Sealed/Program.cs
@@ -0,0 +1,19 @@
+﻿class KakekNenek
+{
+	public string name = "Kakek dan Nenek";
+}
+sealed class BapakIbu : KakekNenek 
+{
+	public int age = 7575;
+}
+class Anak : BapakIbu // error karena class BapakIbu = sealed
+{
+	public string nama = "Anakmu";
+}
+class Program
+{
+	static void Main()
+	{
+		
+	}
+}
