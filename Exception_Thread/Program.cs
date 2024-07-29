9a3ae324e4c93e19828e3a0acfce3b2b0c3a39b3
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-29 15:00:26 +0700

    Exception Thread

---
+++ Exception_Thread/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Company Starting");
+		Thread employee1 = new Thread(Task);
+		employee1.Start();
+		employee1.Join();
+
+		Console.WriteLine("Company Stoped");
+	}
+	static void Task()
+	{	
+		try
+		{
+			throw new Exception("Employee Task");
+		}
+		catch
+		{
+			Console.WriteLine("Clear Task");
+		}
+	}
+}
