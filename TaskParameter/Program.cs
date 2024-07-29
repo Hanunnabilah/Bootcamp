f4750662a97b91109a1f4c3f07b8ddf1a8401b74
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-29 14:56:59 +0700

    Add Task Parameter

---
+++ TaskParameter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Contracts;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Starting");
+		Task t1 = new Task(() => Print("this is message"));
+		t1.Start();
+		Task t2 = new Task(() => Print("this is message"));
+		Console.WriteLine("Finished");
+	}
+	static void Print(string message)
+	{
+		Console.WriteLine("Print start");
+		Thread.Sleep(5000);
+		Console.WriteLine("Print finished" + message);
+	}
+}
