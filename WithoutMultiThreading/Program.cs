554978e69a19a5f21b6d5bcc08a62060d03d7c9f
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-29 14:57:35 +0700

    Program without multithreading

---
+++ WithoutMultiThreading/Program.cs
@@ -0,0 +1,33 @@
+﻿// SYNCHRONOUS (PROGRAM DIJALANKAN BERGANTIAN)
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Program Start Execution");
+		Print();
+		Fax();
+		Scan();
+		Console.WriteLine("Program Finshed Execution");
+		
+	}
+	static void Print()
+	{
+		Console.WriteLine("Print Start");
+		// hold program selama 5 detik
+		Thread.Sleep(5000);
+		Console.WriteLine("Print Finished");
+	}
+	static void Fax()
+	{
+		Console.WriteLine("Fax Start");
+		// hold program selama 10 detik
+		Thread.Sleep(10000);
+		Console.WriteLine("Fax Finished");
+	}static void Scan()
+	{
+		Console.WriteLine("Scan Start");
+		// hold program selama 3 detik
+		Thread.Sleep(3000);
+		Console.WriteLine("Scan Finished");
+	}
+}	
