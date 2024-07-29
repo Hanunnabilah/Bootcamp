b69134329a79635fa0fe55a7618097214d76a259
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-29 14:58:18 +0700

    Thread Exception With Lambda

---
+++ ThreadExceptionWithLambda/Program.cs
@@ -0,0 +1,26 @@
+﻿//ThreadExceptionWithLambda
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Company Starting");
+		Thread employee1 = new Thread(() => {
+			try
+			{
+			MethodA();
+			}
+			catch(Exception e)
+			{
+			
+			}
+		});
+		employee1.Start();
+		employee1.Join();
+
+		Console.WriteLine("Company Stoped");
+	}
+	static void MethodA()
+	{
+		throw new Exception("Employee Task");
+	}
+}
