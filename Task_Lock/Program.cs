2aea331561a6bbaa47fe85404bc0c5af275f4709
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-30 11:26:53 +0700

    Add Task Lock Mechanism when rice condition

---
+++ Task_Lock/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+class Program
+{
+	static int Counter = 0;
+	static object myLock = new();
+	static async Task Main()
+	{
+		Console.WriteLine("Main method started");
+		Task task1 = Task.Run(Incrementer);
+		Task task2 = Task.Run(Incrementer);
+		await Task.WhenAll(task1, task2);
+		Console.WriteLine("Main method completed");
+	}
+	static async Task Incrementer()
+	{
+		for(int i = 0; i<100 ; i++)
+		{
+			lock(myLock)
+			{
+				Counter++;
+				Console.WriteLine(Counter);
+			}
+			await Task.Delay(50);
+		}
+	}
+}
