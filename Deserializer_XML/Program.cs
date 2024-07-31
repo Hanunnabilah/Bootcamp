507664c0ddca0bc86e3e4c288cad0c6d02c0287d
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-31 13:14:27 +0700

    Create Deserializer for XML

---
+++ Deserializer_XML/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+public class Room
+{
+	public int Id{ get; set; }
+	public string Name { get; set; }
+}
+class Program
+{
+	static void Main()
+	{
+		XmlSerializer serializer = new(typeof(Room));
+		Room room;
+		using (FileStream fs = new("./room.txt", FileMode.Open))
+		{
+			room = (Room)serializer.Deserialize(fs);
+		}
+		Console.WriteLine(room.Id);
+		Console.WriteLine(room.Name);
+	}
+}
