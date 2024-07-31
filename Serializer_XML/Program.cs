90abc0e0db9e5ac5ad7156813aee1cebfa1f57bf
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-31 13:15:41 +0700

    Create Serializer for XML

---
+++ Serializer_XML/Program.cs
@@ -0,0 +1,27 @@
+﻿// class must be public
+using System.Dynamic;
+using System.Xml.Serialization;
+
+public class Room
+{
+	public int Id{ get; set; }
+	public string Name { get; set; }
+}
+class Program
+{
+	static void Main()
+	{
+		Room room = new Room();
+		room.Name = "Meeting Room A";
+		room.Id = 001;
+
+		// for serializer XML
+		XmlSerializer serializer = new(typeof(Room));
+		
+		// for create room.txt
+		using (FileStream fs = new("./room.txt", FileMode.Create))
+		{
+			serializer.Serialize(fs, room);
+		} 
+	}
+}
