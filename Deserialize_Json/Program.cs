3312e0bea59af8a6c12dc778fc6ef717e122a85d
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-07-31 13:14:06 +0700

    Create Deserialize for Json

---
+++ Deserialize_Json/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
+
+public class Room
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public RoomType Type { get; set; }
+
+	public enum RoomType
+	{
+		Large,
+		Medium, 
+		Small
+	}
+	class Program
+	{
+		static void Main()
+		{
+			string result;
+			using (StreamReader sr = new("./roomList.json"))
+			{
+				result = sr.ReadLine();
+			}
+			Room room = JsonSerializer.Deserialize<Room>(result);
+			Console.WriteLine(room.Id);
+			Console.WriteLine(room.Name);
+		}
+	} 
+}
+
