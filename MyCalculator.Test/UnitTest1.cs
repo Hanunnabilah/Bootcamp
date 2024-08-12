e8c1768f063218e1dee668995f839e7b3756d2f9
Hanun <nabilah.hanun@m3.formulatrix.com>
2024-08-12 11:33:25 +0700

    Calculator Test

--- MyCalculator.Test/UnitTest1.cs
+++ MyCalculator.Test/UnitTest1.cs
@@ -1,17 +1,36 @@
 namespace MyCalculator.Test;
 
-public class Tests
+public class MyCalculatorTest
 {
-	private MyCalculator myCalculator;
+	private MyCalculator _calculator;
 	[SetUp]
 	public void Setup()
 	{
-		MyCalculator = new MyCalculator();
+		_calculator = new MyCalculator();
 	}
 
 	[Test]
-	public void Test1()
+	public void Add_ReturnCorrectResult()
 	{
-		Assert.Pass();
+		// Arrange
+		int x = 3;
+		int y = 3;
+		int expected = 6;
+		// Act
+		int result = _calculator.Add(x, y);
+		// Assert
+		Assert.That(result, Is.EqualTo(expected));
+	}
+	[TestCase(3,2,5)]
+	[TestCase(-3,-4,-7)]
+	[TestCase(3,-1,2)]
+	[TestCase(2,4,6)]
+	public void Add_ReturnCorrectResult(int x, int y, int expected)
+	{
+		// Arrange
+		// Act
+		int result = _calculator.Add(x,y);
+		// Assert
+		Assert.That(result, Is.EqualTo(expected)); 
 	}
 }
